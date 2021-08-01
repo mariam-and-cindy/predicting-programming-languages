53f395e0ce9c5b2ae06ede8431da7639ebbe5390
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-07-31 19:07:16 -0500

    add prepare

---
+++ prepare.py
@@ -0,0 +1,281 @@
+import unicodedata
+import re
+import json
+
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import acquire as a
+from textblob import TextBlob
+
+
+#import
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+
+
+def clean_df(df):
+    '''
+    takes in a df and lowercase everything, normalize unicode characters, replace anything that is not a letter,
+    number, whitespace or a single quote.
+    retunr a clean df
+    '''
+    for column in df.columns:
+        #get lowercase
+        df[column]= df[column].str.lower()
+        #normalize
+        df[column]= [unicodedata.normalize('NFKC',df[column][n])\
+                     .encode('ascii', 'ignore')\
+                    .decode('utf-8') for n in range (0, len(df))]
+        #remove special characters
+        df[column]= [re.sub(r"[^a-z0-9\s]", '', df[column][n] ) for n in range (0, len(df))]
+    return df
+
+
+
+
+def basic_clean (string):
+    '''
+    takes in a string and lowercase everything, normalize unicode characters, replace anything that is not a letter,
+    number, whitespace or a single quote.
+    retunr a clean string
+    '''
+    
+    string = string.lower()
+    string = unicodedata.normalize('NFKC',string)\
+    .encode('ascii', 'ignore')\
+    .decode('utf-8')
+    string = re.sub(r"[^a-z0-9\s]", '', string)
+    string = re.sub(r'\w*http\w*', '', string)
+    string = re.sub(r'\w*github\w*', '', string)
+    string = re.sub(r'\w*html\w*', '', string)
+    return string
+
+
+def tokenize (string):
+    '''
+    take in a string and tokenize all the words in the string
+    '''
+    
+    # Create the tokenizer
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    # Use the tokenizer
+    string = tokenizer.tokenize(string, return_str = True)
+    return string
+
+
+def stem (string):
+    '''
+     takes in a text and return the text after applying stemming to all the words.
+    '''
+    # Create porter stemmer.
+    ps = nltk.porter.PorterStemmer()
+    # Apply the stemmer to each word in our string.
+    stems = [ps.stem(word) for word in string.split()]
+    text_stemmed = ' '.join(stems)
+    return text_stemmed 
+
+
+
+def lemmatize (text):
+    '''
+     Takes in some text and return the text after applying lemmatization to each word.
+    '''
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    # Use the lemmatizer on each word in the list of words we created by using split.
+    lemmas = [wnl.lemmatize(word) for word in text.split()]
+    
+    # Join our list of words into a string again; assign to a variable to save changes.
+    text_lemmatized = ' '.join(lemmas)
+    
+    return text_lemmatized
+    
+
+
+def remove_stopwords (string, extra_words= [], exclude_words=[]):
+    '''
+    Takes in a strand return the text after removing all the stopwords.
+    Parameters:
+    string : text in  string type
+    extra_words : list of additional stop words to include,
+    exclude_words : list of any words that we don't want to remove.
+    '''
+    # standard English language stopwords list from nltk
+    stopword_list = stopwords.words('english')
+    
+    #add extra_words
+    stopword_list =stopword_list + extra_words
+    
+    #exclude words
+    for element in exclude_words:
+        if element in stopword_list:
+            stopword_list.remove(element)
+    # Split words.
+    words = string.split()
+    
+    # Create a list of words from my string with stopwords removed and assign to variable.
+    filtered_words = [word for word in words if word not in stopword_list]
+    
+    # Join words in the list back into strings; assign to a variable to keep changes.
+    string_without_stopwords = ' '.join(filtered_words)
+    
+    return string_without_stopwords
+
+
+
+def prepare_data(df, column, extra_words=[], exclude_words=[]):
+    '''
+    This function take in a df and the string name for a text column with 
+    option to pass lists for extra_words and exclude_words and
+    returns a df with the  original text, cleaned (tokenized and stopwords removed) , stemmed text,
+    lemmatized text.
+    '''
+    df['clean'] = df[column].apply(basic_clean)\
+                            .apply(tokenize)\
+                            .apply(remove_stopwords, 
+                                   extra_words=extra_words, 
+                                   exclude_words=exclude_words)
+    
+    df['stemmed'] = df['clean'].apply(stem)
+    
+    df['lemmatized'] = df['clean'].apply(lemmatize)
+    
+    return df
+
+
+
+
+def miss_dup_values(df):
+    '''
+    takes in a dataframe of observations and attributes and returns a dataframe where each row is an atttribute name, 
+    the first column is the number of rows with missing values for that attribute, 
+    and the second column is percent of total rows that have missing values for that attribute and 
+    duplicated rows.
+    '''
+        # Total missing values
+    mis_val = df.isnull().sum()
+        # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+        #total of duplicated
+    dup = df.duplicated().sum()  
+        # Percentage of missing values
+    dup_percent = 100 * dup / len(df)
+        # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+    mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+        # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
+           " columns that have missing values.")
+    print( "  ")
+    # print (f"** There are {dup} duplicate rows that represents {round(dup_percent, 2)}% of total Values**")
+        # Return the dataframe with missing information
+    return mis_val_table_ren_columns
+
+
+def remove_nonenglish (df):
+    '''
+    takes in df and 1 column to check if the text is in englis if not that row is going to be remove
+    '''
+    for n in range (0, len(df)):
+        text = df.readme_contents[n]
+        lang = TextBlob(text)
+        if lang.detect_language() != 'en':
+            df =df.drop([n])
+    return df.reset_index(drop=True)
+
+def top_n_target(df,target,  n):
+    #get the value counts of the target
+    targ =pd.DataFrame(df[[target]].value_counts())\
+    .reset_index().rename(columns= {0:'cnt', 'index':target})
+    #get the top 5
+    topl= list(targ.loc[0:(n-1)].language.values)
+    #get new df with only the top n values of target
+    df= df[df.language.isin(topl)].reset_index()
+    return  df
+
+def prepare_mf (df):
+    '''
+    takes in a df and all the rows with missing information, non English text,
+    and then clean, tokenize, stemming, lemmatize
+    '''
+    #removing missing values
+    df = df.dropna(axis=0).reset_index(drop=True)
+
+    #removing texts that are not English
+    df = remove_nonenglish(df)
+
+    #replace Jupyter notebook by python
+    df['language'].replace('Jupyter Notebook', 'Python', inplace=True )
+
+    #get the top 5 programming languages
+    df = top_n_target(df, 'language', 5)
+
+
+    #use my prepare function to  clean, tokenized, stemming, lemmatize
+    df =prepare_data(df, 'readme_contents')
+
+
+    return df.reset_index(drop=True)
+    
+
+
+def split_data(df, target):
+    '''
+        This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes
+    
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate[target])
+    
+    
+    print(f'complete df -> {df.shape}')
+    print(f'train -> {train.shape}')
+    print(f'validate -> {validate.shape}')
+    print(f'test -> {test.shape}')
+
+    return train, validate, test
+
+
+def split_Xy (train, validate, test, target):
+    '''
+    This function takes in three dataframe (train, validate, test) and a target  and splits each of the 3 samples
+    into a dataframe with independent variables and a series with the dependent, or target variable.
+    The function returns 3 dataframes and 3 series:
+    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test.
+    Example:
+    X_train, y_train, X_validate, y_validate, X_test, y_test = split_Xy (train, validate, test, 'Fertility' )
+    '''
+    
+    #split train
+    X_train = train.drop(columns= [target])
+    y_train= train[target]
+    #split validate
+    X_validate = validate.drop(columns= [target])
+    y_validate= validate[target]
+    #split validate
+    X_test = test.drop(columns= [target])
+    y_test= test[target]
+
+    print(f'X_train -> {X_train.shape}               y_train->{y_train.shape}')
+    print(f'X_validate -> {X_validate.shape}         y_validate->{y_validate.shape} ')        
+    print(f'X_test -> {X_test.shape}                  y_test>{y_test.shape}') 
+    return  X_train, y_train, X_validate, y_validate, X_test, y_test
